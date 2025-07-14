18a6f4e981a247fd56d979248ca3af3307ec3df3
Qhonda <omarhuaytarachoque@gmail.com>
2025-07-13 21:54:40 -0500

    logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
-session_destroy();
-header("Location: login.php");
+session_start();        // Inicia sesión para acceder a $_SESSION
+session_unset();        // Limpia todas las variables de sesión
+session_destroy();      // Destruye la sesión
+header("Location: login.php"); // Redirige al login (o donde quieras)
 exit;
+?>
